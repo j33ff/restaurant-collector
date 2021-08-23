0c9324aa2cddebbf808272751bc1f7b1c58d8e0c
Jeff FitzGerald <j2fitzgerald@gmail.com>
2021-08-23 14:17:08 -0700

    adding restaurant detail page

--- restaurantcollector/main_app/models.py
+++ restaurantcollector/main_app/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+class Restaurant(models.Model):
+    name = models.CharField(max_length=100)
+    cuisine = models.CharField(max_length=100)
+    description = models.TextField(max_length=250)
+    city = models.CharField(max_length=100)
+    country = models.CharField(max_length=100)
+    stars = models.IntegerField()
+    def __str__(self):
+        return self.name
+
+
--- restaurantcollector/main_app/views.py
+++ restaurantcollector/main_app/views.py
@@ -1,26 +1,16 @@
 from django.shortcuts import render
+from .models import Restaurant
 
 # Create your views here.
 from django.http import HttpResponse
-
-class Restaurant:
-    def __init__(self, name, cuisine, description, city, country, stars):
-        self.name = name
-        self.cuisine = cuisine
-        self.description = description
-        self.city = city
-        self.country = country
-        self.stars = stars
-
-restaurants = [
-    Restaurant('Minami', 'Japanese', 'Amazing Salmon Oshi','Vancouver', 'Canada', 5),
-    Restaurant('Maenam', 'Thai', 'Delicious Steamed Local Clams','Vancouver', 'Canada', 4),
-    Restaurant('Sing Sing','Fusion', 'Fantastic beer selection and pizza','Vancouver', 'Canada', 4),
-]
 
 def home(request):
     return HttpResponse('<h1>Restuarant Collector</h1>')
 def about(request):
   return render(request, 'about.html')
 def restaurants_index(request):
+  restaurants = Restaurant.objects.all()
   return render(request, 'restaurants/index.html', { 'restaurants': restaurants })
+def restaurants_detail(request, restaurant_id):
+  restaurant = Restaurant.objects.get(id=restaurant_id)
+  return render(request, 'restaurants/detail.html', { 'restaurant': restaurant })
